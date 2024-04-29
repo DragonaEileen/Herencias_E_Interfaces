ae973ac49afd71ad24ce0940d3e154377e5a78d0
emiralles <emiralles@106-07.info2>
2024-04-29 14:32:38 +0200

    Miau miau miau

--- HerenciasInterfaces/src/zoo/Cat.java
+++ HerenciasInterfaces/src/zoo/Cat.java
@@ -9,16 +9,39 @@ public class Cat extends Mammal{
 	/**
 	 * Breed of the cat
 	 */
-	enum catBreed{
-		Sphynx,
-		Siamese,
-		Persian
-	};
+	String catBreed;
 	
 	/**
 	 * Name of the cat
 	 */
 	String catName;
 	
-
+	/* Constructor */
+	/**
+	 * Constructor with Parameters of a Wild Cat
+	 * 
+	 * @param catBreed Breed of the cat
+	 */
+	public Cat(String catBreed) {
+		
+		/* If the cat is wild, the super.domestic keeps being false */
+		this.catBreed = catBreed;
+		
+	}//Fin constructor (breed)
+	
+	/**
+	 * Constructor WITH ALL Parameters
+	 * 
+	 * @param catBreed Breed of the cat
+	 * @param catName Name of a domestic cat
+	 */
+	public Cat(String catBreed, String catName) {
+		
+		/* If the cat has a name, it's a domestic cat */
+		this.catBreed = catBreed;
+		this.catName = catName;
+		super.domestic = true;
+		
+	}//Fin Constructor (Breed, Name)
+	
 }
